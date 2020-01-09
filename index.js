c29002d8652a4ea92188549652eeea8c59e678d7
Valentin Placido <valentinplacido25@gmail.com>
2020-01-09 03:37:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(num) {
   var arr = [];
+  let i = 0;
   do {
     arr.push("I love the Beatles!");
+    i++;
   } while( increment() < num);
 }
